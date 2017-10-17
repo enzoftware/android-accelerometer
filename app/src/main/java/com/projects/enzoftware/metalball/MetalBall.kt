c62ea26d62ce85d866cb8daf5ac09493dcd86684
enzoftware <lizama.enzo@gmail.com>
2017-10-17 00:56:02 -0500

    kotlin sensors

--- app/src/main/java/com/projects/enzoftware/metalball/MetalBall.kt
+++ app/src/main/java/com/projects/enzoftware/metalball/MetalBall.kt
@@ -2,6 +2,7 @@ package com.projects.enzoftware.metalball
 
 import android.content.Context
 import android.content.pm.ActivityInfo
+import android.graphics.Bitmap
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
@@ -9,9 +10,8 @@ import android.hardware.SensorManager
 import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.view.Window
-import android.view.WindowManager
+import android.os.Vibrator
+import android.view.*
 
 class MetalBall : AppCompatActivity() , SensorEventListener {
 
@@ -58,6 +58,71 @@ class MetalBall : AppCompatActivity() , SensorEventListener {
         }
     }
 
+    override fun onResume() {
+        super.onResume()
+        mSensorManager!!.registerListener(this,mAccelerometer,
+                                        SensorManager.SENSOR_DELAY_GAME)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mSensorManager!!.unregisterListener(this)
+    }
+
+}
 
 
+class GroundView(context: Context?) : SurfaceView(context), SurfaceHolder.Callback{
+
+    // ball coordinates
+    var cx : Float = 10.toFloat();
+    var cy : Float = 10.toFloat();
+
+    // last position increment
+
+    var lastGx : Float = 0.toFloat();
+    var lastGy : Float = 0.toFloat();
+
+    // graphic size of the ball
+
+    var picHeight: Int = 0
+    var picWidth : Int = 0
+
+    var icon:Bitmap ?= null
+
+    // window size
+
+    var Windowwidth : Int = 0
+    var Windowheight : Int = 0
+
+    // is touching the edge ?
+
+    var noBorderX = false
+    var noBorderY = false
+
+    var vibratorService : Vibrator ?= null
+    var thread : DrawThread ?= null
+
+
+
+    init {
+        holder.addCallback(this)
+        //create a thread
+        thread = DrawThread(holder,this)
+        // get references and sizes of the objects
+
+        var display : Display =
+    }
+
+    override fun surfaceChanged(holder: SurfaceHolder?, format: Int, width: Int, height: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun surfaceDestroyed(holder: SurfaceHolder?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun surfaceCreated(holder: SurfaceHolder?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
